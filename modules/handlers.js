771d2e059da5b67276d4e78d99a59c5dae4a787a
Yulber Espinoza <yulbertime@gmail.com>
2024-10-26 19:00:59 -0500

    commit final

---
+++ modules/handlers.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const Book = require("../models/book");
+
+const Handler = {};
+
+Handler.getBooks = async (request, response, next) => {
+  try {
+    const books = await Book.find({});
+    console.log(books);
+    response.status(200).json(books);
+  } catch (error) {
+    console.log(error);
+    //response.status(400).send('no hemos encontrado libros ');
+    next(error);
+  }
+};
+
+Handler.createBook = async (request, response, next) => {
+  try {
+    const book = await Book.create(resquest.body);
+    response.status(200).send(book);
+  } catch (error) {
+    error.customMessage = "estamos teniendo problemas para crear el book:";
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
+Handler.deleteBook = async (request, response, next) => {
+  try {
+    console.log("Id Book a borrar:", request.params.id);
+    await Book.findByIdAndDelete(request.params.id);
+    response.sendStatus(204);
+  } catch (error) {
+    error.customMessage =
+      "tenemos un problema cuando queremos borrar su libro:";
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
+module.exports = Handler;
--- server.js
+++ server.js
@@ -1,18 +1,56 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const getBooks = require("./modules/handlers");
+const Handler = require("./modules/handlers");
+
+require("./dataBase");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// agregando mongoose
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Conectado a MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a MongoDB", err);
+  });
 
-  response.send('test request received')
+//conectando la base de datos
 
-})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("we connected");
+});
 
+// routers
+app.get("/books", Handler.getBooks);
+app.post("/books", Handler.createBook);
+app.delete("/books/:id", Handler.deleteBook);
+app.get("*", (request, response) => {
+  response.status(404).send("Sorry,Esta pagina no existe");
+});
+
+app.use((error, request, response, next) => {
+  response
+    .status(500)
+    .send(
+      `Muy mal, ocurrio un error en el servidor ... ${error.customMessage} ${error.message} `
+    );
+});
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
